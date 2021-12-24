3f6fa42e0d932df06e6da4d8ccca82cf265c3a1c
Elmehdi Bairouk <mehdi.bairouk1998@gmail.com>
2021-12-24 02:38:46 +0100

    brief3

---
+++ index.js
@@ -0,0 +1,6 @@
+function loader() {
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector(".loader").style.transition = "2s";
+    document.querySelector(".header").style.display = "block";
+    document.querySelector(".section").style.display = "block";
+  }
